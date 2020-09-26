c7207814a3afeee871a9c02d0ad9a00c637f5153
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-25 21:54:55 -0400

    Add redux-logger middleware to redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import './index.css'
 import App from './components/App'
 import reducer from './redux/reducers'
@@ -22,7 +23,7 @@ function ColorfulBorder() {
 }
 
 // Create redux store.
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <React.StrictMode>

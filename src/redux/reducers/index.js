b8bbd30e91a16609f2565b084d5b87cb3829708f
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-25 21:45:18 -0400

    Declare root reducer using combineReducers method

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+
+import { combineReducers } from 'redux';
+import tweets from './tweets';
+import users from './users';
+import authedUser from './authedUser';
+
+export default combineReducers({
+  users,
+  tweets,
+  authedUser
+});
+

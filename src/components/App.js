2929b6d868bb8b763b269a07305fb20681e7082a
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-25 22:53:21 -0400

    Dispatch handleGetInitialData thunk action creator within App component's useEffect hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,16 @@
 import * as React from 'react'
+import { useDispatch } from 'react-redux';
+import handleGetInitialData from '../redux/actions/shared'
 
 export default function App () {
+  // Gain access to redux store's dispatch method.
+  const dispatch = useDispatch();
+
+  // Fetch all users and tweets from backend API.
+  React.useEffect(() => {
+    dispatch(handleGetInitialData());
+  }, [dispatch]);
+
   return (
     <div className='container'>
       Redux Course Curriculum

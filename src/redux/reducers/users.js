61796088ab61e37ab176a16da85e69bcff434e53
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-25 22:37:41 -0400

    Add case for SUCCESS_INIT_DATA to users reducer

--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -1,7 +1,14 @@
+import { SUCCESS_INIT_DATA } from '../actions/shared';
+
 // Users reducer.
 const users = (state = {}, action) => {
   switch(action.type) {
-
+    case SUCCESS_INIT_DATA:
+      const { users } = action.data;
+      return {
+        ...state,
+        ...users
+      }
     default:
       return state;
   }

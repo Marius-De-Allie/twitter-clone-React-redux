dc0cd33eb42818bbe8fcfe57fdb14fd0471a4c4c
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-25 22:40:58 -0400

    Add case for SUCCESS_INIT_DATA to tweets reducer

--- src/redux/reducers/tweets.js
+++ src/redux/reducers/tweets.js
@@ -1,7 +1,14 @@
+import { SUCCESS_INIT_DATA } from '../actions/shared';
+
 // Tweets reducer.
 const tweets = (state = {}, action) => {
   switch(action.type) {
-
+    case SUCCESS_INIT_DATA:
+      const { tweets } = action.data;
+      return {
+        ...state,
+        ...tweets
+      }
     default:
       return state;
   }

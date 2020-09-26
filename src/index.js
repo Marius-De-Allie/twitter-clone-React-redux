f90bf471ca42787ae8cf13c0e8f15e602f62d196
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-25 21:50:56 -0400

    Create redux store within index.js file, wrap App component in React-redux's Provider component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import './index.css'
 import App from './components/App'
+import reducer from './redux/reducers'
 
 function ColorfulBorder() {
   return (
@@ -17,10 +21,15 @@ function ColorfulBorder() {
   )
 }
 
+// Create redux store.
+const store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDOM.render(
   <React.StrictMode>
     <ColorfulBorder />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )

19758feb0f89eded787c21857ef790791613a714
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-25 23:47:34 -0400

    Access tweets peice of redux store state within TweetItem component

--- src/components/TweetItem.js
+++ src/components/TweetItem.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const TweetItem = ({ tweet }) => (
-  <li>
-    <div style={{border: 'solid 1px red', margin: '20px'}}>
-      <p>{tweet.author}</p>
-      <p>{tweet.timestamp}</p>
-      <p>{tweet.text}</p>
-      <div className='icons-container'>
-        <span>{`replies: ${tweet.replies.length}, `}</span><span>{`likes: ${tweet.likes.length}`}</span>
+const TweetItem = ({ tweet }) => {
+
+  const tweets = useSelector((state) => state.tweets);
+
+  // Retreive author of tweet being replied to.
+  let author;
+  if(tweet.replyingTo) {
+    author = tweets[tweet.replyingTo].author;
+  };
+
+  return (
+    <li>
+      <div style={{border: 'solid 1px red', margin: '20px'}}>
+        <p>{tweet.author}</p>
+        <p>{tweet.timestamp}</p>
+        {tweet.replyingTo && <p>{`replying to: @${author}`}</p>}
+        <p>{tweet.text}</p>
+        <div className='icons-container'>
+          <span>{`replies: ${tweet.replies.length}, `}</span><span>{`likes: ${tweet.likes.length}`}</span>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default TweetItem;

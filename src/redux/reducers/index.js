49ff207fbedddeafd534b04f7ef76c2cf1b519e4
Marius-De-Allie <spectre4300s@yahoo.com>
2020-10-06 22:54:49 -0400

    Update redux root reducer to include error piece of state

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 import tweets from './tweets';
 import users from './users';
 import authedUser from './authedUser';
+import error from './error';
 
 export default combineReducers({
   users,
   tweets,
-  authedUser
+  authedUser,
+  error
 });
 

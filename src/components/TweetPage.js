4409318c842104a2cfcd623a458e485c9bc0e483
Marius-De-Allie <spectre4300s@yahoo.com>
2020-10-07 18:17:07 -0400

    Create new TweetPage functional component

---
+++ src/components/TweetPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TweetItem from './TweetItem';
+
+const TweetPage = ({ match }) => (
+  <div>
+    TweetItem
+    ReplayForm
+  </div>
+);
+
+export default TweetPage;

e8343e80a6433ef59c45e5f3107ce01f80ef50f9
Marius-De-Allie <spectre4300s@yahoo.com>
2020-10-12 22:38:51 -0400

    Access tweet from redux store's tweets piece of state within TweetPage component

--- src/components/TweetPage.js
+++ src/components/TweetPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import TweetItem from './TweetItem';
 
-const TweetPage = ({ match }) => (
-  <div>
-    TweetItem
-    ReplayForm
-  </div>
-);
+const TweetPage = ({ match }) => {
+  // Retrieve current tweet from redux store.
+  const tweet = useSelector((state) => state.tweets[match.params.tweetId]);
+
+  return (
+    <div>
+      {tweet.id}
+      TweetItem
+      ReplayForm
+    </div>
+  );
+};
 
 export default TweetPage;

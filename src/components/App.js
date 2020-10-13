7e637cb418cffa8c70d8916e902e36c92e67a82c
Marius-De-Allie <spectre4300s@yahoo.com>
2020-10-12 22:49:06 -0400

    Update tweet Route's component prop value to TweetPage within App component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import handleGetInitialData from '../redux/actions/shared';
 import Homepage from './Homepage';
+import TweetPage from './TweetPage';
 
 export default function App () {
   // Gain access to redux store's dispatch method.
@@ -18,7 +19,7 @@ export default function App () {
       <Router>
         <Switch>
           <Route path="/" exact component={Homepage} />
-          <Route path="/tweet/:tweetId" render={() => <p>TweetPage</p>} />
+          <Route path="/tweet/:tweetId" component={TweetPage} />
           <Route path="/new" render={() => <p>NewTweetPage</p>} />
           <Route render={() => <p>404 Page</p>} />
         </Switch>

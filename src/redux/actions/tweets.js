586772101093d84b0aefe07085275392d8dd66a0
Marius-De-Allie <spectre4300s@yahoo.com>
2020-10-14 22:37:04 -0400

    Declare addReply action creator and conditional dispatch addReplay action within handleAddTweet thunk action creator

--- src/redux/actions/tweets.js
+++ src/redux/actions/tweets.js
@@ -3,6 +3,7 @@ import { saveLikeToggle, saveTweet } from '../../utils/api';
 // ACTION TYPES
 const TOGGLE_LIKE = 'TOGGLE_LIKE';
 const ADD_TWEET = 'ADD_TWEET';
+const ADD_REPLY = 'ADD_REPLY';
 
 // ACTION CREATORS.
 const toggleLike = ({ id, hasLiked, authedUser }) => ({
@@ -18,6 +19,12 @@ const addTweet = (tweet) => ({
   tweet
 });
 
+const addReply = (tweetId, replyId) => ({
+  type: ADD_REPLY,
+  tweetId,
+  replyId
+});
+
 /*************TODO: UPDATE THUNK ACTION CREATOR TO DO OPTIMISTIC UPDATE ********************/
 // THUNK ACTION CREATORS.
 const handleToggleLike = (toggleObj) => {
@@ -31,13 +38,18 @@ const handleToggleLike = (toggleObj) => {
   }
 };
 
-const handleAddTweet = (tweetInfo) => {
+const handleAddTweet = (id, tweetInfo) => {
   return(dispatch) => {
     // call async api function to save new tweet.
     saveTweet(tweetInfo)
     // Dispatch plain action object to add tweet if request successful.
       .then((tweet) => {
         dispatch(addTweet(tweet));
+        // 1. Check whether the new tweet is a reply to an existing tweet.
+        if(tweet.replyingTo !== null) {
+          // 2. if so, dispatch ADD_REPLY action.
+          dispatch(addReply(id, tweet.id));
+        }
       })
       .catch((e) => console.log(e));
   }
@@ -47,5 +59,6 @@ export {
   handleToggleLike as default,
   handleAddTweet,
   TOGGLE_LIKE,
-  ADD_TWEET
+  ADD_TWEET,
+  ADD_REPLY
 };

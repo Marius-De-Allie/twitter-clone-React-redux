ba3139b615353db8c5adaa23a274cd0a533f6f66
Marius-De-Allie <spectre4300s@yahoo.com>
2020-10-15 23:47:25 -0400

    Declare repliesArray IIFE within TweetPage component, IIFE returns a sorted array of current tweet's replies

--- src/components/TweetPage.js
+++ src/components/TweetPage.js
@@ -4,8 +4,18 @@ import TweetItem from './TweetItem';
 import ReplyForm from './ReplyForm';
 
 const TweetPage = ({ match }) => {
-  // Retrieve current tweet from redux store.
+  // Retrieve current tweet from redux store state.
   const tweet = useSelector((state) => state.tweets[match.params.tweetId]);
+  // Retrieve current tweets peice of state from redux store.
+  const tweets = useSelector((state) => state.tweets);
+
+  let replies;
+  const repliesArray = (() => {
+    if(tweet) {
+      replies = tweet.replies.map(replyId => tweets[replyId])
+        .sort((a, b) => b.timestamp - a.timestamp);
+    }
+  })();
 
   return (
     <div>
@@ -13,7 +23,7 @@ const TweetPage = ({ match }) => {
         <React.Fragment>
           <TweetItem tweet={tweet} />
           <ReplyForm />
-
+          
         </React.Fragment>
         ) 
         : <h2>Loading</h2>
